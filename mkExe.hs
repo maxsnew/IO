330c6248ac9b86fc51242f7fa73632f94636411a
Max New <maxsnew@gmail.com>
2014-06-09 17:22:19 -0500

    Allow elm flags

--- mkExe.hs
+++ mkExe.hs
@@ -11,6 +11,9 @@ import           System.Directory
 import           Elm.Internal.Paths as Elm
 import qualified Paths_ElmIO as ElmIO
 
+shareFile :: FilePath -> IO FilePath
+shareFile = ElmIO.getDataFileName . ("share" </>)
+
 catToFile :: [FilePath] -> FilePath -> IO ()
 catToFile files outfile = do
 	writeFile outfile ""
@@ -25,22 +28,22 @@ buildJS code infile outfile = case code of
     exitWith code
   ExitSuccess -> do
     putStrLn "Making exe"
-    prescript <- ElmIO.getDataFileName "prescript.js"
-    handler   <- ElmIO.getDataFileName "handler.js"
+    prescript <- shareFile "prescript.js"
+    handler   <- shareFile "handler.js"
     catToFile [ prescript
               , Elm.runtime
               , "build" </> replaceExtension infile "js"
               , handler ]
               outfile
 
-compile :: FilePath -> IO ExitCode
-compile infile = do
+compile :: FilePath -> [String] -> IO ExitCode
+compile infile elmFlags = do
   dir <- ElmIO.getDataDir
-  rawSystem "elm" ["-mo", "--src-dir=" ++ dir, infile]
+  rawSystem "elm" $ ["-mo", "--src-dir=" ++ dir] ++ elmFlags ++ [infile]
 
 addImports :: FilePath -> IO ()
 addImports src = do
-  imports <- ElmIO.getDataFileName "imports.elm"
+  imports <- shareFile "imports.elm"
   imports' <- readFile imports
   srcFile <- strictRead src
   let (firstLine:rest) = lines srcFile 
@@ -50,35 +53,37 @@ addImports src = do
           string <- readFile file 
           length string `seq` return string
 
+parseArgs :: [String] -> IO (Bool, [String], FilePath, FilePath)
+parseArgs raw = case raw of
+  [] -> usageExit
+  [_] -> usageExit
+  ("--default-ports"):inF:outF:rest -> return (True, rest, inF, outF)
+  inF:outF:rest -> return (False, rest, inF, outF)
+  where usageExit = usage >> (exitWith $ ExitFailure 1)
+
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [infile, outfile] -> do
-      code <- compile infile
-      buildJS code infile outfile
-    [flag, infile, outfile] -> do
-      case flag of
-        "--default-ports" -> do
-          -- Adding boilerplate to a temp file
-          ports <- ElmIO.getDataFileName "boilerplate.elm"
-          (src,handle) <- openTempFile "" infile
-          hClose handle
-          catToFile [infile, ports] src
-          addImports src
+  rawArgs <- getArgs
+  (defaultPorts, elmFlags, infile, outfile) <- parseArgs rawArgs
+  if not defaultPorts
+    then do code <- compile infile elmFlags
+            buildJS code infile outfile
+    else do -- Adding boilerplate to a temp file
+            ports <- shareFile "boilerplate.elm"
+            (src,handle) <- openTempFile "" infile
+            hClose handle
+            catToFile [infile, ports] src
+            addImports src
 
-          -- Build output js file
-          code <- compile src
-          buildJS code src outfile
+            -- Build output js file
+            code <- compile src elmFlags
+            buildJS code src outfile
 
-          -- Cleanup
-          removeFile src
-          removeFile $ "build" </> replaceExtension src "js"
-          removeFile $ "cache" </> replaceExtension src "elmi"
-          removeFile $ "cache" </> replaceExtension src "elmo"
-
-        _ -> usage
-    _ -> usage
+            -- Cleanup
+            removeFile src
+            removeFile $ "build" </> replaceExtension src "js"
+            removeFile $ "cache" </> replaceExtension src "elmi"
+            removeFile $ "cache" </> replaceExtension src "elmo"
 
 usage :: IO ()
-usage = putStrLn $ "USAGE: mkExe [--default-ports] <input-file> <output-file> [--default-ports]"
+usage = putStrLn $ "USAGE: mkExe [--default-ports] <input-file> <output-file> [elm-flags]*"
